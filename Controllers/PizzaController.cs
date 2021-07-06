131d0e68151fd200d75ecf343aea25179b02a338
Tik <tikgithub@gmail.com>
2021-07-06 09:03:40 +0700

    First Commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using DotnetContoso.Models;
+using DotnetContoso.Service;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+        }
+
+        // GET all action
+
+        // GET by Id action
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
